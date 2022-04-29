aed56139d590f09c93a7ff74c75c7e0697dde835
Temik-hub <surshkov.art@gmail.com>
2022-04-29 13:35:05 +0300

    init

---
+++ dist/precache-manifest.8fc6a68c60e2794a48f7c04d4f905451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a26aa836e47a6e8975090cfee93a7d8",
+    "url": "/marvel/index.html"
+  },
+  {
+    "revision": "c16f894e2648e9d7fa01",
+    "url": "/marvel/js/app.ee86b815.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/marvel/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/marvel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/marvel/robots.txt"
+  }
+]);
